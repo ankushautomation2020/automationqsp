b56465157538f73e1ff9f5ac5c697bec7d850a73
admin <admin@DESKTOP-94GO3MR>
2021-09-16 09:18:59 +0530

    My First Commit

---
+++ automationqsp/src/qsp/wcsm5NEW.java
@@ -0,0 +1,12 @@
+package qsp;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class wcsm5NEW {
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver","./drivers/chromedriver.exe");
+		new ChromeDriver();
+	}
+
+}
