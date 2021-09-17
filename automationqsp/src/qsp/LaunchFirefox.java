449aa1dc21af199f8bf42c94735b6a7d9440d568
ankushautomation2020 <90815454+ankushautomation2020@users.noreply.github.com>
2021-09-17 09:20:45 +0530

    Update LaunchFirefox.java

--- automationqsp/src/qsp/LaunchFirefox.java
+++ automationqsp/src/qsp/LaunchFirefox.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 public class LaunchFirefox {
 
 	public static void main(String[] args) {
-		System.setProperty("webdriver.gecko.driver","./drivers/geckodriver.exe");
+		
 		
 
 	}
